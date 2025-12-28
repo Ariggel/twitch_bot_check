996caca79ad38f7acb541def180e8017643436d9
Sascha <saschaavsn@gmail.com>
2025-12-28 19:25:57 +0100

    feat: :sparkles: extract list of users
    implemented the ability to extract data from a list of users instead of just one user

--- src/functions/pipelines.py
+++ src/functions/pipelines.py
@@ -1,8 +1,17 @@
-import os, requests
+import os, requests, pandas
 from utils import paths
 from functions import credentials, user_data
 
-def extract(username) -> dict:
-    user_dict = user_data.get_user(username)
+def extract(username : str) -> dict:
+    user_dict = user_data.get(username)
     return user_dict
+
+
+def extract_all(username_list : list) -> pandas.DataFrame:
+    data_list = []
     
+    for username in username_list:
+        user_extraction = extract(username)
+        data_list.append(user_extraction)
+    data_dataframe = pandas.DataFrame(data_list)
+    return data_dataframe
--- src/main.py
+++ src/main.py
@@ -1,4 +1,18 @@
 from functions import pipelines
 
-print(pipelines.extract("ariggeldiggel"))
+#print(pipelines.extract("ariggeldiggel"))
+list = [
+"ajgamer111111111111"
+,"alibetispagetti"
+,"amine022__"
+,"andres_kim05"
+,"andresking108"
+,"angrygingesbiggestfan12"
+,"anonymous_aman"
+,"arasil__"
+,"ardaa42_"
+,"axelander_4051"
+,"bediiiiiiiii"
+]
 
+print(pipelines.extract_all(list))
