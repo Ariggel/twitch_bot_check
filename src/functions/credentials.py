0905a8515ba229daf3a5859e38b2c7d2ba4134f2
Sascha <saschaavsn@gmail.com>
2025-12-30 02:35:37 +0100

    feat: :bug: major rework of credentials
    reworked the credentials.py to correctly differ between private and public scope - a user access token will now freshly be generated each time the function is requested

--- src/functions/credentials.py
+++ src/functions/credentials.py
@@ -1,9 +1,8 @@
 import os, requests
 import os
-from utils import load_config
+from utils import load_config, logging_config
 
-
-def get_access_token() -> str:
+def get_access_token(scope : str = "public") -> str:
     """
     Retrieve an App Access Token from the Twitch API using the Client Credentials flow.
 
@@ -30,17 +29,69 @@ def get_access_token() -> str:
           private data (e.g., subscriptions) requires a User Access Token with
           appropriate scopes.
     """
+    #---- collecting all input credentials for generation of the access token ----
     client = os.environ.get('CLIENT')
     client_secret = os.environ.get('CLIENT_SECRET')
-    access_url = load_config.url_access()
+    client_user_token = os.environ.get('CLIENT_USER_TOKEN')
+    redirect_uri = os.environ.get('REDIRECT_URL')
 
-    params_input = {
-         "client_id"        : client
-        ,"client_secret"    : client_secret
-        ,"grant_type"       : "client_credentials"
+    credentials_type = {
+         "public"   : "client_credentials"
+        ,"private"  : "authorization_code"
     }
 
-    access_token_file = requests.post(access_url, data = params_input)
-    access_token_json = access_token_file.json()
+    required_credentials = {
+         "public"   : [client, client_secret, credentials_type.get(scope,[])]
+        ,"private"  : [client, client_secret, credentials_type.get(scope,[]), client_user_token, redirect_uri]
+    }
+
+    if not all(required_credentials.get(scope, [])):
+        logging_config.logger_credentials.critical("client environment variables are not set")
+        raise RuntimeError("missing client credentials")
     
-    return access_token_json["access_token"]
+    access_url = load_config.url_access()
+    
+ # ---- gather the token ----
+    if scope == "public":
+        params_input = {
+             "client_id"        : client
+            ,"client_secret"    : client_secret
+            ,"grant_type"       : credentials_type.get(scope,0)
+        }
+        
+        try:
+            access_token_file = requests.post(access_url, data = params_input)
+            status = access_token_file.status_code
+        except Exception:
+            logging_config.logger_credentials.exception("failed to retrieve access token")
+            logging_config.logger_credentials.exception(f"status code: {status}")
+
+        if status == 200:
+            logging_config.logger_credentials.info("access token generation successfull")
+
+        access_token_json = access_token_file.json()
+        token = access_token_json["access_token"]
+
+    elif scope == "private":
+        params_input = {
+             "client_id"        : client
+            ,"client_secret"    : client_secret
+            ,"code"             : client_user_token
+            ,"grant_type"       : credentials_type.get(scope,0)
+            ,"redirect_uri"     : redirect_uri
+        }
+        
+        try:
+            access_token_file = requests.post(access_url, data = params_input)
+            status = access_token_file.status_code
+        except Exception:
+            logging_config.logger_credentials.exception("failed to retrieve access token")
+            logging_config.logger_credentials.exception(f"status code: {status}")
+
+        if status == 200:
+            logging_config.logger_credentials.info("access token generation successfull")
+
+        access_token_json = access_token_file.json()
+        token = access_token_json["access_token"]
+
+    return token
